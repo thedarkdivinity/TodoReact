516b004638a10208eab15d33d84bb521d4d44ede
Sayush Kamat <thedarkdivinity@users.noreply.github.com>
2020-06-06 00:48:17 +0530

    List items can be added but time cannot

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Todo from './Todo';
+const App=()=>{
+    const[input,setInput]=useState({
+      task:'',
+      waqt:''
+    });
+    const [list,setList]=useState([]);
+    const [listwaqt,setListWaqt]=useState('');
+  const changed=(e)=>{
+    const {name,value}=e.target;
+    if(name==="task"){
+      setInput((old)=>{
+        return{
+          task:value,
+          waqt:old.waqt
+        }
+      })
+    }
+    else if(name==="waqt"){
+      setInput((old)=>{
+        return{
+          task:old.task,
+          waqt:value
+        }
+      })
+    }
+  }
+  const clicked=()=>{
+    setList((prev)=>{
+      return [...prev,input.task];
+    })
+    setInput((curr)=>{
+      return{
+      task:'',
+      waqt:''
+      }
+    });
+    setListWaqt(()=>{
+      return [input.waqt];
+    })
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  }
+  return(
+    <>
+      <h1>Enter tasks</h1>
+      <input type="text" placeholder="tasks likho" name="task" onChange={changed} value={input.task}/>
+      <br/>
+      <br/>
+      <br/>
+      <input type="date" placeholder="date likho" name="waqt" onChange={changed} value={input.waqt}/>
+      <button onClick={clicked}>Add IT</button>
+      <ol>
+      {list.map((val)=>{
+        return(
+          <Todo text={val}/> 
+        )
+      })
+    
+    }
+      
+      
+      </ol>
+    </>
+
   );
 }
-
 export default App;
